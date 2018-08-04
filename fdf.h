66969da332191d8f240ce4b140eedc54724613ab
CastorLemythe <luka-91@hotmail.fr>
2018-08-05 00:28:39 +0200

    gg

--- fdf.h
+++
@@ -1,9 +0,0 @@
-#ifndef FDF_H
-# define FDF_H
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include "libft.h"
-#endif
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <fdf.h>
-
-int     main(int argc, char **argv)
-{
-    
-
-    return (0);
-}
